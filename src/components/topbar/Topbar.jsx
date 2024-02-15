30dbb97410538034ec88bbf6eb5de83ef7fecc7b
ThomassMathisen <thomasmathisen@hotmail.com>
2024-02-15 19:11:35 +0100

    Basic design of topbar component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,18 @@
 import styles from "./topbar.module.css";
+import { MdOutlineSettings } from "react-icons/md";
 
 const Topbar = () => {
-  return <div className={styles.container}>Topbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.feeds}>
+        <span className={styles.forYou}>For you</span>
+        <span className={styles.following}>Following</span>
+        <span className={styles.settings}>
+          <MdOutlineSettings />
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Topbar;
